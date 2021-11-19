a95bea9db190102c26209d193b452cd64ff172b6
tompoon <tompoon@astri.org>
2021-11-19 11:41:53 +0800

    finish union set

--- algorithmbasic2020/src/class14/Code05_UnionFind.java
+++ algorithmbasic2020/src/class14/Code05_UnionFind.java
@@ -67,4 +67,73 @@ public class Code05_UnionFind {
 		}
 
 	}
+
+
+
+	//my code
+	public static class myNode<T>{
+		T value;
+		public myNode(T value){
+			this.value = value;
+		}
+	}
+
+
+	public static class myUnionFind<V>{
+		private HashMap<V, Node<V>> nodes;
+		private HashMap<Node<V>, Node<V>> parents;
+		private HashMap<Node<V>, Integer> sizeMap;
+
+		public myUnionFind(List<V> values){
+			this.nodes = nodes;
+			this.parents = parents;
+			this.sizeMap = sizeMap;
+			for(V value : values){
+				Node<V> node = new Node<>(value);
+				nodes.put(value, node);
+				parents.put(node, node);
+				sizeMap.put(node, 1);
+			}
+		}
+
+		public Node<V> findFather(Node<V> cur){
+			Stack<Node<V>> s = new Stack<>();
+			while(cur != parents.get(cur)){
+				s.push(cur);
+				cur = parents.get(cur);
+			}
+			while(!s.isEmpty()){
+				parents.put(s.pop(), cur);
+			}
+			return cur;
+		}
+
+		public boolean isSameSet(V a, V b){
+			return findFather(nodes.get(a)) == findFather(nodes.get(b));
+		}
+
+		public int sets(){
+			return sizeMap.size();
+		}
+
+		public void union(V a, V b){
+			Node<V> ahead = findFather(nodes.get(a));
+			Node<V> bhead = findFather(nodes.get(b));
+			if(ahead != bhead){
+				int aSize = sizeMap.get(ahead);
+				int bSize = sizeMap.get(bhead);
+				Node<V> largerhead = aSize > bSize ? ahead : bhead;
+				Node<V> smallerhead = largerhead == ahead ? bhead : ahead;
+				parents.put(smallerhead, largerhead);
+				sizeMap.put(largerhead, aSize + bSize);
+				sizeMap.remove(smallerhead);
+			}
+		}
+
+	}
+
+
+
+
+
 }
